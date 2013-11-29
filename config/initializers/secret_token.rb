8ff79cf2fb7cad97099f51a1912ce9ace87adfb9
Ralph Reid <beresfordjunior@me.com>
2013-11-29 09:49:04 +0000

    intitial app build

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BirdDogSandboxGrabberEver::Application.config.secret_token = '3a76f080de2e787ebe87cdcc46bfe8c03d5fa1baf4b57fd650520f2d4107ca68ac07e82f941c91ff621d8ab86582378999fb94dc9e7d1602efc19e48f4a0662f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+BirdDogSandboxGrabberEver::Application.config.session_store :cookie_store, key: '_Bird_Dog_SandBox-Grabber_Ever_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# BirdDogSandboxGrabberEver::Application.config.session_store :active_record_store
