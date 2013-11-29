1b9d3440fb47bcf6ee8b8a036ac61b23bde0cd20
Ralph Reid <beresfordjunior@me.com>
2013-11-29 10:07:56 +0000

    app up and running

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,5 @@
+class HomeController < ApplicationController
+  def index
+    @hello = "Ralph Mon"
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 BirdDogSandboxGrabberEver::Application.routes.draw do
+  get "home/index"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
